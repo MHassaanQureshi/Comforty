70f471dbe5ae038e481bbe757d877bebaef0b864
MHassaanQureshi <156497587+MHassaanQureshi@users.noreply.github.com>
2024-12-08 11:29:10 +0500

    Add files via upload

--- app/components/Partners/Partners.tsx
+++ app/components/Partners/Partners.tsx
@@ -7,13 +7,13 @@ export default function Partners(){
         <div
   
   className="flex flex-row w-[90%] p-4 justify-center items-center rounded gap-4 flex-wrap bg-[#F0F2F3] md:gap-10">
-  <Partnerimg link="/images/logo-7.png"/>
-  <Partnerimg link="/images/logo-1.png"/>
-  <Partnerimg link="/images/logo-2.png"/>
-  <Partnerimg link="/images/logo-3.png"/>
-  <Partnerimg link="/images/logo-4.png"/>
-  <Partnerimg link="/images/logo-5.png"/>
-  <Partnerimg link="/images/logo-6.png"/>
+  <Partnerimg link="/images/Logo-7.png"/>
+  <Partnerimg link="/images/Logo-1.png"/>
+  <Partnerimg link="/images/Logo-2.png"/>
+  <Partnerimg link="/images/Logo-3.png"/>
+  <Partnerimg link="/images/Logo-4.png"/>
+  <Partnerimg link="/images/Logo-5.png"/>
+  <Partnerimg link="/images/Logo-6.png"/>
   
 </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,10 @@ export default function Home(){
     <div className="w-full flex flex-col p-2 mt-10">
       <h1 className="text-2xl font-bold p-2">Featured Products</h1>
       <div className=" w-[full] flex flex-col items-center mt-5 gap-4 md:flex-row md:justify-center md:gap-10">
-      <NewProduct img="/images/Product-1.png" price={20} name="Library Stool Chair" />
-      <SaleProductcard img="/images/Product-2.png" name="Library Stool Chair" from={30} price={20}/>
-      <NotSaleProduct img="/images/Product-3.png" name="Library Stool Chair" price={20}/>
-      <NotSaleProduct img="/images/Product-4.png" name="Library Stool Chair" price={20}/>
+      <NewProduct img="/images/product-1.png" price={20} name="Library Stool Chair" />
+      <SaleProductcard img="/images/product-2.png" name="Library Stool Chair" from={30} price={20}/>
+      <NotSaleProduct img="/images/product-3.png" name="Library Stool Chair" price={20}/>
+      <NotSaleProduct img="/images/product-4.png" name="Library Stool Chair" price={20}/>
     
       </div>
     </div>
